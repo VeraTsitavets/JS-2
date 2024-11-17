2c4ea14336ed509c8d6e30bae9c431907737fd8c
VeraTsitavets <tsitavetsva@gmail.com>
2024-11-17 16:12:58 +0300

    hehe

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -4,6 +4,29 @@
  * Результат сохраните в переменной grade.
  */
 
-const score = 70 // тестовое значение, можно изменять
+const score = 74 // тестовое значение, можно изменять
 let grade
 // your code
+if (score >= 0 && score <= 100) {
+
+    if ( score >= 90 && score <= 100) {
+        grade = 'A'
+    
+        } else if(score >= 80 && score <= 89){
+        grade = 'B'
+    
+        } else if(score >= 70 && score <= 79){
+        grade = 'C'
+        
+        } else if(score >= 50 && score <= 69){
+        grade = 'D'
+        
+        } else {
+        grade = 'F'
+        
+        }
+} else {
+    console.log('Ошибка')
+}
+
+console.log(grade);
