2cb9f6655f759a1a08333f3f4a9e131f53ce6a74
VeraTsitavets <tsitavetsva@gmail.com>
2024-11-24 15:54:16 +0300

    yeah

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,17 +1,7 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-/* function calculateAverage(firstNumber, secondNumber) {
-    if (firstNumber >= 0 && secondNumber >= 0) {
-        console.log((firstNumber + secondNumber) / 2);
-        
-    } else {
-        console.log(umError);
-        
-    }
-} */
-
     function calculateAverage(firstNumber, secondNumber) {
             console.log((firstNumber + secondNumber) / 2)
     }
-    
+
 calculateAverage(23, 45)
