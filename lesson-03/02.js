8bacb468c66efab263d06e13344f329a7cd81b10
VeraTsitavets <tsitavetsva@gmail.com>
2024-11-24 21:09:04 +0300

    yyyyyy

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,4 +1,24 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
-function isEven() {}
+function isEven(yourNumber) {
+    let answer
+
+    if (yourNumber % 2 ) {
+        let answer = true
+        console.log(answer);
+    }else{
+        let answer = false
+        console.log(answer);
+    }
+
+    return answer
+}
+
+isEven (1)
+isEven (2)
+isEven (3)
+isEven (4)
+isEven (5)
+isEven (6)
+isEven (7)
