e4ec1a030df008cc5cba9896b523afdc6a23eed5
VeraTsitavets <tsitavetsva@gmail.com>
2024-11-25 13:16:12 +0300

    new_oneee

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -2,13 +2,13 @@
 
 function findLargest(num_1, num_2, num_3) {
     let theBiggestOne
-    if (num_1 > num_2 || num_1 > num_3) {
+    if (num_1 >= num_2 && num_1 >= num_3) {
         theBiggestOne = num_1
         console.log(theBiggestOne);    
-    }else if(num_2 > num_1 || num_2 > num_3){
+    }else if(num_2 >= num_1 && num_2 >= num_3){
         theBiggestOne = num_2
         console.log(theBiggestOne)
-    }else if(num_3 > num_2 || num_3 > num_1){
+    }else if(num_3 >= num_2 && num_3 >= num_1){
         theBiggestOne = num_3
         console.log(theBiggestOne)
     }else{
@@ -17,7 +17,7 @@ function findLargest(num_1, num_2, num_3) {
     return theBiggestOne
 } 
 
-findLargest(5, 3, 2)
+findLargest(1, 2, 3)
 findLargest(2, 5, 2)
 findLargest(1, 2, 5)
 findLargest(5, 2, 5)
