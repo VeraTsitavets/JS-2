8d0b37cf0b7c84f974cc93de59dd32be5a311a15
VeraTsitavets <tsitavetsva@gmail.com>
2024-11-25 13:10:08 +0300

    new_oneee

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,23 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(num_1, num_2, num_3) {
+    let theBiggestOne
+    if (num_1 > num_2 || num_1 > num_3) {
+        theBiggestOne = num_1
+        console.log(theBiggestOne);    
+    }else if(num_2 > num_1 || num_2 > num_3){
+        theBiggestOne = num_2
+        console.log(theBiggestOne)
+    }else if(num_3 > num_2 || num_3 > num_1){
+        theBiggestOne = num_3
+        console.log(theBiggestOne)
+    }else{
+        console.log('ermm what')
+    }
+    return theBiggestOne
+} 
+
+findLargest(5, 3, 2)
+findLargest(2, 5, 2)
+findLargest(1, 2, 5)
+findLargest(5, 2, 5)
