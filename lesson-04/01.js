c795e6d62dac87ab65af975d9447094460d32005
VeraTsitavets <tsitavetsva@gmail.com>
2024-12-03 21:11:07 +0300

    ttt

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -13,7 +13,7 @@
 includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 */
 
-function includesElement(array, element) {
+/*function includesElement(array, element) {
 
     for (let index = 0; index < array.length; index++) {
 
@@ -35,18 +35,37 @@ function includesElement(array, element) {
 }
 
 
-    includesElement([1, 2, 3], 4)
+    includesElement([1, 2, 3], 4) */
+
+    /* const meow = [1, 2, 3] //['true', 'false']
+    for (let index = 0; index < meow.length; index++) {
+        const meowMeow = meow[index];
+        
+        if (meow[index] == 'false') {
+            answer = true
+        }else{
+            answer = false
+        }
+        console.log(answer);
+        return(answer)
+        
+    } */
 
 
-    /*     for (let index = 0; index < array.length; index++) {
+function includesElement(array, element) {
+    for (let index = 0; index < array.length; index++) {
 
         if (array[index] === element) {
 
             answer = true
-            
+            break
         } else {
+
             answer = false
         } 
     }
     console.log(answer)
-} */
+}
+    
+    includesElement([1, 2, 3], 1)
+    includesElement([true, false], false)
