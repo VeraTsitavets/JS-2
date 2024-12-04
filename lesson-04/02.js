32acde0df08f227e014b5e215f25b5a4acc9060b
VeraTsitavets <tsitavetsva@gmail.com>
2024-12-04 15:49:35 +0300

    111111

--- lesson-04/02.js
+++ lesson-04/02.js
@@ -15,4 +15,64 @@
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-function findUniqueElements() {}
+function findUniqueElements(array) {
+
+    const newArray = []
+    let doubleNumbers = 0
+
+    for (let index = 0; index < array.length; index++) {
+        let okayWrong = array[index]
+    
+        if (doubleNumbers === array.indexOf[index]) {
+            doubleNumbers++
+            console.log(doubleNumbers);
+        } else {
+            array.splice(index, 1)
+            console.log(array);
+
+            newArray.push(array)
+            console.log(newArray);
+            break
+        } 
+    }
+    //console.log(newArray)
+    //return(newArray)
+}
+
+
+findUniqueElements([1, 2, 3, 2, 1, 4])  
+
+/*const array = [1, 2, 3, 2, 1, 4]
+const newArray = []
+let doubleNumbers = 0
+const doubleNumbersArray = []
+
+
+for (let index = 0; index < array.length; index++) {
+    const okayThat = array[index];
+    //console.log(okayThat);
+
+    for (let doubleNumbers = 0; doubleNumbers < array.length; doubleNumbers++) {
+        const element = array[doubleNumbers];
+        
+    }
+    if (array[index] !== array[doubleNumbers]) {
+        doubleNumbersArray.push(array[index])
+        console.log(doubleNumbersArray);
+        
+    }
+
+    /* if (array[index] === okayThat) {
+        doubleNumbersArray.push(okayThat)
+        array.splice(array[index], 1)
+        console.log(doubleNumbersArray);
+        console.log(array);
+    }else if(array[index] !== okayThat) {
+        console.log( array = ' yay');
+        
+    } */
+    
+
+
+//}     //console.log(newArray)
+    //return(newArray)
--- lesson-04/03.js
+++ lesson-04/03.js
@@ -17,4 +17,27 @@ findCommonElements([1, 2, 3], [2, 3, 4]) // [2, 3]
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-function findCommonElements() {}
+function findCommonElements(array1, array2) {
+    const newShit = []
+
+    for (let index1 = 0; index1 < array1.length; index1++) {
+        const sameNum1 = array1[index1];
+        for (let index2 = 0; index2 < array2.length; index2++) {
+            const sameNum2 = array2[index2];
+            if (sameNum1 === sameNum2) {
+                newShit.push(sameNum1)  
+                console.log(newShit);
+                return newShit              
+            }else{
+                //console.log(newShit);
+                return newShit
+            }
+        }
+    }
+
+
+
+}
+
+
+findCommonElements([1, 2, 3], [2, 3, 4])
