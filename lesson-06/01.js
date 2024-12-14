10f03b5148c24f02f7e60ca1aa5740dd9acdfc7e
VeraTsitavets <tsitavetsva@gmail.com>
2024-12-14 15:13:28 +0300

    uuughhhh

--- lesson-06/01.js
+++ lesson-06/01.js
@@ -17,3 +17,20 @@ const WEB_TECH_IMAGES = [
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
 ]
+
+const buttonPrev = document.querySelector('#prev-button')
+const buttonNext = document.querySelector('#next-button')
+const imagesHTML = document.querySelector('#web-tech-image')
+let i = 0
+
+
+  buttonPrev.addEventListener('click', function() {
+    i > 0 && i <= WEB_TECH_IMAGES.length - 1 ? i-- : i = 0;
+    imagesHTML.src = WEB_TECH_IMAGES[i]
+  })
+
+  buttonNext.addEventListener('click', function() {
+    i >= 0 && i < WEB_TECH_IMAGES.length - 1 ? i++ : i = 0;
+    imagesHTML.src = WEB_TECH_IMAGES[i]
+
+  })
